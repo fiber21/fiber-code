96c384253f6ac9229119b830a487c23c8a827aff
fiber21 <102272060+fiber21@users.noreply.github.com>
2022-08-28 11:32:21 +0700

    Update guess.php

--- guess.php
+++ guess.php
@@ -1,8 +1,14 @@
-<?php  
+<?=  
 // this file contains php code to guess birthday
 // file ini berisi kode untuk menebak hari lahir seseorang berdasarkan tanggal lahir yg mereka berikan
 
+// first we want to use date ()
+// date() have two parameters first u can put letter like l or d
+// "l" stand for day and "d" stand for date since we want to search the day we put l string as parameter
+// the second parameter we can put the strtotime function
+// this function has parameter like now , tomorrow, and yesterday but the cool thing is you can just type date month year
 
+date("l", strtotime("26 mar 2001"));
 
 
 
